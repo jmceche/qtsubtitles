9dcc68064dd3b6bfc250a9219051cd2d9129938e
juan Martin Cechetto <jmcechetto@outlook.com>
2020-11-28 13:59:52 -0300

    bug fixes

--- settings.py
+++
@@ -1,9 +0,0 @@
-# settings.py
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
-SECRET_KEY = os.environ.get("OPENSUBTITLE_USERAGENT")
